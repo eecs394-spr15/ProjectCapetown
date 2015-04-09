6885e0ca45ebe49557b7429dfa7e0a3df65def9d
Bingyu Wang <bingyuwang91@gmail.com>
2015-04-08 21:28:35 -0500

    Homepage1.1

--- app/Home/scripts/IndexController.js
+++ app/Home/scripts/IndexController.js
@@ -1,4 +1,4 @@
-angular
+  angular
   .module('Home')
   .controller('IndexController', function($scope, supersonic) {
     // Controller functionality here
@@ -7,7 +7,7 @@ angular
     	name: 'The Book of Trees',
     	price: 19,
     	pubdate: new Date('2014', '03', '08'),
-    	cover: 'img/the-book-of-trees.jpg',
+    	cover: '/imags/InfantCognitionLab.jpg',
     	likes: 0,
     	dislikes: 0
   	},
@@ -15,9 +15,34 @@ angular
     	name: 'Program or be Programmed',
     	price: 8,
     	pubdate: new Date('2013', '08', '01'),
-    	cover: 'img/program-or-be-programmed.jpg',
+    	cover: '/imags/ReberLab.jpg',
     	likes: 0,
     	dislikes: 0
   	}
     ];
   });
+  /*
+  angular
+    .module('Home', [])
+    .controller('IndexController', function($scope, supersonic) {
+      // Controller functionality here
+      $scope.products = [
+    	{
+      	name: 'The Book of Trees',
+      	price: 19,
+      	pubdate: new Date('2014', '03', '08'),
+      	cover: 'app/common/assets/imags/InfantCognitionLab.jpg',
+      	likes: 0,
+      	dislikes: 0
+    	},
+    	{
+      	name: 'Program or be Programmed',
+      	price: 8,
+      	pubdate: new Date('2013', '08', '01'),
+      	cover: 'app/common/assets/imags/ReberLab.jpg',
+      	likes: 0,
+      	dislikes: 0
+    	}
+      ];
+    });
+    */
