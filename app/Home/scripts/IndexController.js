0e26f15f0edf45a84dde36825ff328a26375ec2a
Bingyu Wang <bingyuwang91@gmail.com>
2015-04-08 21:58:48 -0500

    home1.2

--- app/Home/scripts/IndexController.js
+++ app/Home/scripts/IndexController.js
@@ -4,23 +4,41 @@
     // Controller functionality here
     $scope.products = [
   	{
-    	name: 'The Book of Trees',
-    	price: 19,
-    	pubdate: new Date('2014', '03', '08'),
-    	cover: '/imags/InfantCognitionLab.jpg',
-    	likes: 0,
-    	dislikes: 0
+      lab: 'Visual Attention and Cognition Lab',
+      study: 'Programmer needed',
+      duration: 'N/A',
+      compensation: 'work study',
+      location: ' Cresap Hall 301',
+    	cover: '/imags/VisCogLabProgrammer.jpg'
   	},
   	{
-    	name: 'Program or be Programmed',
-    	price: 8,
-    	pubdate: new Date('2013', '08', '01'),
-    	cover: '/imags/ReberLab.jpg',
-    	likes: 0,
-    	dislikes: 0
+      lab: 'Visual Cognition Lab',
+      study: 'Spatial Navigation study',
+      duration: '45 mins',
+      compensation: '$10',
+      location: '112 Cresap Hall',
+    	cover: '/imags/VisCogLabBehavioral.jpg'
+  	},
+    {
+      lab: 'Infant Cognition Lab',
+      study: 'Object Perception study',
+      duration: '60 mins',
+      compensation: '$20',
+      location: '110 Cresap Hall',
+    	cover: '/imags/InfantCognitionLab.jpg'
   	}
     ];
   });
+
+  /*
+  name: 'Program or be Programmed',
+  price: 8,
+  pubdate: new Date('2013', '08', '01'),
+  cover: '/imags/ReberLab.jpg',
+  likes: 0,
+  dislikes: 0
+
+  */
   /*
   angular
     .module('Home', [])
