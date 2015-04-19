73555dd61b13634150c2018fc626126123a45529
Jingyao Qin <Jingyaoq@gmail.com>
2015-04-19 11:51:18 -0500

    add sign up page

---
+++ app/Home/scripts/NewuserController.js
@@ -0,0 +1,30 @@
+angular
+  .module('Home')
+  .controller("NewuserController", function ($scope, Event, supersonic) {
+    $scope.event = {};
+
+    $scope.submitForm = function () {
+      $scope.showSpinner = true;
+      newevent = new Event($scope.event);
+
+        var user = new Parse.User();
+        user.set("username", "my name");
+        user.set("password", "my pass");
+        user.set("email", "email@example.com");
+ 
+        user.signUp(null, {
+            success: function(user) {
+                // Hooray! Let them use the app now.
+            },
+            error: function(user, error) {
+                // Show the error message somewhere and let the user try again.
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+    };
+
+    $scope.cancel = function () {
+      supersonic.ui.modal.hide();
+    }
+
+  });
