4685bcf1056543af6b5de0d52e1e78a53daa773f
Jingyao Qin <Jingyaoq@gmail.com>
2015-04-19 19:35:10 -0500

    sign up function done

--- app/Home/scripts/NewuserController.js
+++ app/Home/scripts/NewuserController.js
@@ -1,30 +1,33 @@
 angular
-  .module('inputExample', [])
-  .controller("NewuserController", function ($scope, supersonic) {
-    $scope.user = {};
-
-    $scope.submit = function() {
-//        $scope.showSpinner = true;
-
+  .module('Home',[])
+  .controller("NewuserController", ['$scope', function ($scope) {
+    $scope.user = { Username: "", Password: "", Email: ""};
+      
+    $scope.addUser = function($scope) {
+     
         var user = new Parse.User();
-        user.set("username", $scope.user.name);
-        user.set("password", $scope.user.password);
-        user.set("email", $scope.user.email);
+        user.set("username", this.user.Username);
+        user.set("password", this.user.Password);
+        user.set("email", this.user.Email);
+        
+//        var user = new Parse.User();
+//        user.set("username", "name");
+//        user.set("password", "password");
+//        user.set("email", "email@gmail.com");
  
         user.signUp(null, {
             success: function(user) {
+                document.write("2");
+                alert("A new user Created!");
                 // Hooray! Let them use the app now.
                 window.location.href='settings.html';
             },
             error: function(user, error) {
+                document.write("3");
                 // Show the error message somewhere and let the user try again.
                 alert("Error: " + error.code + " " + error.message);
+                window.location.href='settings.html';
             }
         });
     };
-
-//    $scope.cancel = function () {
-//      supersonic.ui.modal.hide();
-//    }
-
-  });
+  }]);
