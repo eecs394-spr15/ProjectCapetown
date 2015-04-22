e0b9c62a550cd12ae1166e754852e6962458e571
Bingyu Wang <bingyuwang91@gmail.com>
2015-04-21 20:15:24 -0500

    Add the vote function(with a minor bug)

--- app/Home/scripts/IndexController.js
+++ app/Home/scripts/IndexController.js
@@ -2,14 +2,14 @@
   .module('Home')
   .controller('IndexController', function($scope, supersonic) {
     // Controller functionality here
-            
+
 //         var TestObject = Parse.Object.extend("event");
 //         var query = new Parse.Query(TestObject);
 //         query.limit(10);
 //         query.find({
 //                 success: function(results) {
 //       document.getElementById("1").innerHTML = "Successfully retrieved " + results.length + " events.";
-//     for (var i = 0; i < results.length; i++) { 
+//     for (var i = 0; i < results.length; i++) {
 //       var object = results[i];
 //         document.getElementById("2").innerHTML = document.getElementById("2").innerHTML + '<br>' + object.get('location') + ' - ' + object.get('study')+' - ' + object.get('cover');
 
@@ -20,7 +20,8 @@
 //     document.getElementById("1").innerHTML = "error";
 //   }
 // });
-      
+
+/*
     $scope.posters = [
   	{
       lab: 'Visual Cognition Lab',
@@ -48,38 +49,6 @@
   	}
     ];
   });
+*/
 
-  /*
-  name: 'Program or be Programmed',
-  price: 8,
-  pubdate: new Date('2013', '08', '01'),
-  cover: '/imags/ReberLab.jpg',
-  likes: 0,
-  dislikes: 0
-
-  */
-  /*
-  angular
-    .module('Home', [])
-    .controller('IndexController', function($scope, supersonic) {
-      // Controller functionality here
-      $scope.posters = [
-    	{
-      	name: 'The Book of Trees',
-      	price: 19,
-      	pubdate: new Date('2014', '03', '08'),
-      	cover: 'app/common/assets/imags/InfantCognitionLab.jpg',
-      	likes: 0,
-      	dislikes: 0
-    	},
-    	{
-      	name: 'Program or be Programmed',
-      	price: 8,
-      	pubdate: new Date('2013', '08', '01'),
-      	cover: 'app/common/assets/imags/ReberLab.jpg',
-      	likes: 0,
-      	dislikes: 0
-    	}
-      ];
-    });
-    */
+  
