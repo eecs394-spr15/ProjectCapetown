88d6dcc9bad638d82f1bb0fdbd0ec2b006f54b5b
BenBenernstein <benjaminbernstein2015@u.northwestern.edu>
2015-04-22 12:59:40 -0500

    StudyInfo module

---
+++ app/studyInfo/index.js
@@ -0,0 +1,4 @@
+angular.module('studyInfo', [
+  // Declare any module-specific AngularJS dependencies here
+  'common'
+]);
---
+++ app/studyInfo/scripts/IndexController.js
@@ -0,0 +1,5 @@
+angular
+  .module('studyInfo')
+  .controller('IndexController', function($scope, supersonic) {
+    // Controller functionality here
+  });
