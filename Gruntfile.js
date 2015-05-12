7cabc5f772e3f778f66e0248b706b20da30c41c5
lukeahn1224 <ahnnuga@gmail.com>
2015-05-11 19:39:23 -0500

    steroids

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,14 @@
 /*
- * Default Gruntfile for AppGyver Steroids
- * http://www.appgyver.com
- *
- * Licensed under the MIT license.
- */
 
-'use strict';
+Default Gruntfile for AppGyver Steroids
+http://www.appgyver.com
+Licensed under the MIT license.
+
+*/
 
 module.exports = function(grunt) {
-
   grunt.loadNpmTasks("grunt-steroids");
-
-  grunt.registerTask("default", ["steroids-make", "steroids-compile-sass"]);
-
-};
+  grunt.registerTask("default", [
+    "steroids-make-fresh"
+  ]);
+}
