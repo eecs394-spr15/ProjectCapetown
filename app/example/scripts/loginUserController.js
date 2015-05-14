1921340771d564f6d51cab476ec48e1cfa63ee64
Jingyao Qin <Jingyaoq@gmail.com>
2015-05-14 12:13:23 -0500

    login&signup done

---
+++ app/example/scripts/loginUserController.js
@@ -0,0 +1,46 @@
+angular
+  .module('example',[])
+  .controller("loginUserController", ['$scope', function ($scope) {
+    $scope.login = { Username: "", Password: ""};
+//    $scope.continue = function($scope) {
+//        supersonic.ui.initialView.dismiss();
+//    }
+    $scope.loginUser = function($scope) {
+        
+        var name = this.user.Username;
+        var password = this.user.Password;
+        if(localStorage.getItem(name)) {
+            loginStorage();
+        } else {
+            alert("User name not found");
+        }
+        function loginStorage() {
+            var currentPassword = localStorage.getItem(name);
+            if(currentPassword == password){
+                supersonic.ui.initialView.dismiss();
+            }else{
+                alert("Password not match");
+            }
+        }     
+        
+    };
+
+    $scope.addUser = function($scope) {
+        var name = this.user.Username;
+        var password = this.user.Password;
+        if(!localStorage.getItem(name)) {
+            signupStorage();
+        } else {
+            alert("User name has been used, please try another");
+        }
+        function signupStorage() {
+            localStorage.setItem(name, password);
+            var currentPassword = localStorage.getItem(name);
+            if(currentPassword == password){
+                supersonic.ui.initialView.dismiss();
+            }else{
+                alert("sign up failed");
+            }
+        }
+    };
+  }]);
