c462d7ba7224197ba4134c1129583efb3f4ac708
Jingyao Qin <Jingyaoq@gmail.com>
2015-05-17 22:11:15 -0500

    real case data

--- dist/app/example.js
+++ dist/app/example.js
@@ -18,6 +18,39 @@ angular
   });
 
 angular
+	.module('example')
+	.controller('caseController', function ($scope)
+	{
+		//$scope.answers = [{label:"a1", val:false}, {label:"a2", val:false}, {label:"a3", val:false}, {label:"a4", val:false}];
+
+		$scope.test = {value:"12345"};
+
+/*
+		$scope.testFunc = function()
+		{
+			//var selected = $filter("filter")($scope.answers, {val:true});
+
+			alert("TEST");
+			return false;//($scope.answers[0].val && $scope.answers[2].val && !($scope.answers[1].val));
+		};
+*/
+/*
+		$scope.selection = ['a1', 'a4'];
+
+		$scope.toggleSelecton = function toggleSelection(answer)
+		{
+			var index = $scope.selection.indexOf(answer);
+
+
+			if(index > -1)
+				$scope.selection.splice(index, 1);
+			else
+				$scope.selection.push(answer);
+
+		};
+*/
+	});
+angular
   .module('example',[])
   .controller("loginUserController", ['$scope', function ($scope) {
     $scope.login = { Username: "", Password: ""};
