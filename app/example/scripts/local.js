59d4df2119cd4f32aa51dea03589527589e8c08f
lukeahn1224 <ahnnuga@gmail.com>
2015-05-26 21:08:39 -0500

    please work

---
+++ app/example/scripts/local.js
@@ -0,0 +1,59 @@
+      angular.module('app', [
+        'ngStorage'
+      ]).
+      
+      controller('local', function(
+        $scope,
+        $localStorage
+      ){
+        $scope.$storage = $localStorage.$default({
+          case10: false,
+          case10q1: false,
+          case10q2: false,
+          case10q3: false,
+          case10q4: false,
+          case10q5: false,
+          case10q6: false,
+          case10q7: false,
+          case11: false,
+          case11q1: false,
+          case11q2: false,
+          case11q3: false,
+          case11q4: false,
+          case11q5: false,
+          case11q6: false,
+          case11q7: false,
+          case12: false,
+          case12q1: false,
+          case12q2: false,
+          case12q3: false,
+          case12q4: false,
+          case12q5: false,
+          case12q6: false,
+          case12q7: false,
+          case12q8: false,
+          case13: false,
+          case13q1: false,
+          case13q2: false,
+          case13q3: false,
+          case13q4: false
+        });
+        
+        $scope.deleteCase10 = function() {
+          delete $scope.$storage.case10;
+        };
+
+        $scope.deleteCase11 = function() {
+          delete $scope.$storage.case11;
+        };
+
+        $scope.deleteCase12 = function() {
+          delete $scope.$storage.case12;
+        };
+
+        $scope.deleteCase13 = function() {
+          delete $scope.$storage.case13;
+        };
+
+        });
+
